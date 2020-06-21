51ee097b85052bdb94aa43a110d41f6e9bff65d1
Florian Bergmann <florian.bergmann@leanbakery.com>
2020-06-21 11:30:16 +0200

    use fallback address 2

--- app/controllers/spree/api/v2/storefront/amazon_controller.rb
+++ app/controllers/spree/api/v2/storefront/amazon_controller.rb
@@ -231,7 +231,7 @@ class  Spree::Api::V2::Storefront::AmazonController < Spree::Api::V2::Storefront
         address_params[:address1] = (amazon_address.address1 || spree_user_address.try(:address1)).to_s + ' ' + (amazon_address.address2 || spree_user_address.try(:address2)).to_s
       end
     else
-      address_params[:address1] = amazon_address.address1 || spree_user_address.try(:address1)
+      address_params[:address1] = amazon_address.address2 || spree_user_address.try(:address2)
     end
 
     if spree_address_book_available?
